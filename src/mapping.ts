1d47b70bbde1c8b8fc7f1fd94c65ef8019596fc3
Irving Cabello <irvollo@gmail.com>
2020-10-15 20:28:49 -0500

    Initial commit with Tokens, Transfers and Transactions

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,23 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
-  Contract,
+  GToken,
   Approval,
   BurnLiquidityPoolPortion,
   CancelLiquidityPoolMigration,
   CompleteLiquidityPoolMigration,
   InitiateLiquidityPoolMigration,
   OwnershipTransferred,
-  Transfer
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  Transfer as TransferEvent,
+  DepositCall
+  // Transfer
+} from "../generated/GToken/GToken"
+
+import { Transfer, Transaction, Token } from '../generated/schema';
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  /* let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
@@ -33,7 +36,7 @@ export function handleApproval(event: Approval): void {
   entity.spender = event.params.spender
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save() */
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -106,4 +109,77 @@ export function handleInitiateLiquidityPoolMigration(
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: TransferEvent): void {
+
+  // Get the transaction if it already exists
+  let transaction = Transaction.load(event.transaction.hash.toHex());
+
+  // Create the transaction if it doesn't 
+  if (transaction == null) {
+    transaction = new Transaction(event.transaction.hash.toHex());
+    transaction.from = event.transaction.from;
+  }
+
+  // Get the Transfer if it already exists
+  let transfer = Transfer.load(event.transactionLogIndex.toHex());
+
+  if (transfer == null) {
+    transfer = new Transfer(event.transactionLogIndex.toHex());
+  }
+
+  // Get the mint token
+  let fromToken = Token.load(event.params.from.toHex());
+
+  if (fromToken == null) {
+    fromToken = new Token(event.params.from.toHex());
+    
+    let Token = GToken.bind(event.params.from);
+
+    if (Token) {
+      fromToken.name = Token.name();
+      fromToken.symbol = Token.symbol();
+    }
+  }
+  
+  transfer.transaction = transaction.id;
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.amount = event.params.value;
+  transfer.fromToken = fromToken.id;
+
+  fromToken.save();
+  transaction.save();
+  transfer.save();
+}
+
+// Deposit
+export function handleDeposit(call: DepositCall): void {
+
+  let entity = Transaction.load(call.transaction.hash.toHex());
+
+  if (entity == null) {
+    entity = new Transaction(call.transaction.hash.toHex());
+  }
+
+  entity.from = call.transaction.from;
+  entity.save();
+}
+
+export function handleInitialBlock(block: ethereum.Block): void {
+  const INITIAL_BLOCK = '21431182';
+  const TOKEN_ADDRESS = '0xD4c84Fc7d3EA365A2824A8C908f93136e625bCeA';
+
+/*   if (INITIAL_BLOCK === block.number.toString()){
+    // Init Block
+    let token = new Token(TOKEN_ADDRESS);
+    token.name = 'gcDAI';
+    token.symbol = 'gcDAI';
+
+    token.save();
+  } */
+  let token = new Token(TOKEN_ADDRESS);
+    token.name = 'gcDAI';
+    token.symbol = 'gcDAI';
+
+    token.save();
+}
